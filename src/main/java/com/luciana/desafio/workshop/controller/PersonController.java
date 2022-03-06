593205f464c89ae760ee8d73d27b932246c8ac2a
luciana <lucianna.adm2010@gmail.com>
2022-03-06 20:12:15 -0300

    Test Web server failed to start.

--- src/main/java/com/luciana/desafio/workshop/controller/PersonController.java
+++ src/main/java/com/luciana/desafio/workshop/controller/PersonController.java
@@ -1,22 +1,34 @@
 package com.luciana.desafio.workshop.controller;
 
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
+import com.luciana.desafio.workshop.contact_of_customer.Person;
+import com.luciana.desafio.workshop.dto.response.MessageResponseDTO;
+import com.luciana.desafio.workshop.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+
 @RestController // aplicações
-@RequestMapping //("/api/v1/people")
+@RequestMapping ("/api/v1/people")
 public class PersonController {
 
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository workshopRepository) {
+        this.personRepository = personRepository;
+    }
+
     @GetMapping
-    public String getVeiculo() {
-        return "Test Commit terminal";
+    public MessageResponseDTO createWorkshop(@RequestBody Person person) {
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message(("Create person withj ID " + savedPerson.getId()))
+                .build();
     }
 }
