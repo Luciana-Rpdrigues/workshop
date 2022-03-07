ecb9a307b8fe9aad172662adecfc5784fc0837c1
luciana <lucianna.adm2010@gmail.com>
2022-03-06 21:38:40 -0300

    Evolution 2

--- src/main/java/com/luciana/desafio/workshop/controller/PersonController.java
+++ src/main/java/com/luciana/desafio/workshop/controller/PersonController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController // aplicações
-@RequestMapping //("/api/v1/people")
+@RequestMapping ("/api/v1/people")
 public class PersonController {
 
     private PersonService personService;
