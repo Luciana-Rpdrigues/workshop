e398437b1d194cff4241a1d50bc92e22171af5f8
luciana <lucianna.adm2010@gmail.com>
2022-03-06 21:34:49 -0300

    Evolution

--- src/main/java/com/luciana/desafio/workshop/dto/request/PhoneDTO.java
+++ src/main/java/com/luciana/desafio/workshop/dto/request/PhoneDTO.java
@@ -1,18 +1,5 @@
 package com.luciana.desafio.workshop.dto.request;
 
-import com.luciana.desafio.workshop.enums.PhoneType;
-
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-
 public class PhoneDTO {
 
-    private Long id;
-
-    @Enumerated(EnumType.STRING)
-    private PhoneType type;
-
-    @NotEmpty
-    @Size(min = 13, max = 14)
-    private String number;
 }
--- src/main/java/com/luciana/desafio/workshop/service/PersonService.java
+++ src/main/java/com/luciana/desafio/workshop/service/PersonService.java
@@ -12,51 +12,16 @@ public class PersonService {
 
     private PersonRepository personRepository;
 
-    private final PersonMapper personMapper = PersonMapper.INSTANCE;
-
-    public MessageResponseDTO createPerson(PersonDTO personDTO) {
-        Person personToSave = personMapper.toModel(personDTO);
-
-        Person savedPerson = personRepository.save(personToSave);
-        return createMessageResponse(savedPerson.getId(), "Created person with ID ");
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
     }
 
-    public List<PersonDTO> listAll() {
-        List<Person> allPeople = personRepository.findAll();
-        return allPeople.stream()
-                .map(personMapper::toDTO)
-                .collect(Collectors.toList());
-    }
-
-    public PersonDTO findById(Long id) throws PersonNotFoundException {
-        Person person = verifyIfExists(id);
-
-        return personMapper.toDTO(person);
-    }
-
-    public void delete(Long id) throws PersonNotFoundException {
-        verifyIfExists(id);
-        personRepository.deleteById(id);
-    }
-
-    public MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException {
-        verifyIfExists(id);
-
-        Person personToUpdate = personMapper.toModel(personDTO);
-
-        Person updatedPerson = personRepository.save(personToUpdate);
-        return createMessageResponse(updatedPerson.getId(), "Updated person with ID ");
-    }
-
-    private Person verifyIfExists(Long id) throws PersonNotFoundException {
-        return personRepository.findById(id)
-                .orElseThrow(() -> new PersonNotFoundException(id));
-    }
-
-    private MessageResponseDTO createMessageResponse(Long id, String message) {
+    public MessageResponseDTO createPerson(Person person) {
+        Person savedPerson = personRepository.save(person);
         return MessageResponseDTO
                 .builder()
-                .message(message + id)
+                .message(("Create person with ID " + savedPerson.getId()))
                 .build();
     }
 }
